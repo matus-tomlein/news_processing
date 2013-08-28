b58b250effda8d13d57fc5568559bddd1c4e34ec
Matus Tomlein <matus@tomlein.org>
2013-08-28 14:03:32 +0200

    Update environment for postgres in production

--- environment/database.go
+++ environment/database.go
@@ -1,5 +1,8 @@
 package environment
 
 func PostgresConnectionString(envType string) (string) {
+	if envType == "production" {
+		return "user=postgres dbname=88mph_production password=noroads sslmode=disable"
+	}
 	return "user=postgres dbname=88mph_development sslmode=disable"
 }

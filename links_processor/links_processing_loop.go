af0c33dc68af34217e032ac87ed82ee315495e1a
Matus Tomlein <matus@tomlein.org>
2013-08-30 21:54:01 +0200

    More efficient links processing

--- links_processor/links_processing_loop.go
+++ links_processor/links_processing_loop.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strconv"
 	"fmt"
+	"strings"
 	"os"
 	"github.com/matus-tomlein/news_processing/page_db"
 	"time"
@@ -20,9 +21,10 @@ func processLinksInUpdates(pageId int, updates []int, ads *page_db.AdsFiltering,
 	ld.RankLinksAndSaveToDb(newUpdates, pageId, envType)
 }
 
-func processUpdatesWithLinksProcessingId(pageId, linksProcessingId int, db *sql.DB, ads *page_db.AdsFiltering, envType string) (int) {
+func processUpdatesWithLinksProcessingId(linksProcessingId int, db *sql.DB, ads *page_db.AdsFiltering, envType string) (int) {
 	currentLinksProcessingIdPath := environment.CurrentLinksProcessingIdPath(envType)
-	updateIds := make([]int, 0)
+	pageUpdates := make(map[int][]int)
+	updatesFound := false
 
 	rows, err := db.Query("select id, page_id from updates where links_processing_id = $1", linksProcessingId)
 	if err != nil {
@@ -30,23 +32,36 @@ func processUpdatesWithLinksProcessingId(pageId, linksProcessingId int, db *sql.
 	}
 	defer rows.Close()
 	for rows.Next() {
+		updatesFound = true
 		var updateId, updatePageId int
+
 		err = rows.Scan(&updateId, &updatePageId)
 		if err != nil { panic(err) }
-		updateIds = append(updateIds, updateId)
-		if pageId < 0 { pageId = updatePageId }
+
+		if updateIds, ok := pageUpdates[updatePageId]; ok {
+			pageUpdates[updatePageId] = append(updateIds, updateId)
+		} else {
+			pageUpdates[updatePageId] = []int { updateId }
+		}
 	}
-	if len(updateIds) > 0 {
+
+	if updatesFound {
+		// Create a temporary file to mark the ongoing process in case of crash
 		err = ioutil.WriteFile(currentLinksProcessingIdPath, []byte(strconv.Itoa(linksProcessingId)), 0644)
 		if err != nil { panic(err) }
 
-		processLinksInUpdates(pageId, updateIds, ads, envType)
+		// Process updates for each page
+		for pageId, updateIds := range pageUpdates {
+			processLinksInUpdates(pageId, updateIds, ads, envType)
+		}
 
+		// Remove the temporary file
 		err = os.Remove(currentLinksProcessingIdPath)
 		if err != nil { panic(err) }
 
 		linksProcessingId++
 	}
+
 	return linksProcessingId
 }
 
@@ -55,7 +70,7 @@ func recoverUnfinishedLinksProcessing(db *sql.DB, ads *page_db.AdsFiltering, env
 	if err != nil { panic(err) }
 	linksProcessingId, err := strconv.Atoi(string(b))
 	if err != nil { panic(err) }
-	processUpdatesWithLinksProcessingId(-1, linksProcessingId, db, ads, envType)
+	processUpdatesWithLinksProcessingId(linksProcessingId, db, ads, envType)
 }
 
 func processLinks(envType string, messages chan string) {
@@ -73,7 +88,7 @@ func processLinks(envType string, messages chan string) {
 	    recoverUnfinishedLinksProcessing(db, ads, envType)
 	}
 
-	pageIds := make([]int, 0)
+	pageIds := make([]string, 0)
 	rows, err := db.Query("select id from pages where priority >= 5")
 	if err != nil {
 		panic(err)
@@ -83,8 +98,12 @@ func processLinks(envType string, messages chan string) {
 		var pageId int
 		err = rows.Scan(&pageId)
 		if err != nil { panic(err) }
-		pageIds = append(pageIds, pageId)
+		pageIds = append(pageIds, strconv.Itoa(pageId))
 	}
+	if len(pageIds) == 0 {
+		panic("No pages to process")
+	}
+	pageIdsString := strings.Join(pageIds, ",")
 
 	linksProcessingId := 0
 	rows, err = db.Query("select COALESCE(max(links_processing_id), 0) AS links_processing_id from updates where links_processing_id IS NOT NULL")
@@ -97,23 +116,21 @@ func processLinks(envType string, messages chan string) {
 	}
 
 	for {
-		for _, pageId := range pageIds {
-			_, err = db.Exec("update updates set links_processing_id = $1 where id in (select id from updates where page_id = $2 and parsed = TRUE and links_processing_id IS NULL order by id limit 100)",
-				linksProcessingId,
-				pageId)
-			if err != nil { panic(err) }
+		_, err = db.Exec("update updates set links_processing_id = $1 where id in (select id from updates where page_id in (" +
+			pageIdsString + ") and parsed = TRUE and links_processing_id IS NULL order by id limit 100)",
+			linksProcessingId)
+		if err != nil { panic(err) }
 
-			linksProcessingId = processUpdatesWithLinksProcessingId(pageId, linksProcessingId, db, ads, envType)
-			time.Sleep(1 * time.Second)
+		linksProcessingId = processUpdatesWithLinksProcessingId(linksProcessingId, db, ads, envType)
 
-			select {
-				case msg := <-messages:
-					if msg == "q" { os.Exit(0) }
-				default:
-			}
+		select {
+			case msg := <-messages:
+				if msg == "q" { os.Exit(0) }
+			default:
 		}
+
 		fmt.Println("All pages processed")
-		time.Sleep(60 * time.Second)
+		time.Sleep(120 * time.Second)
 
 		select {
 			case msg := <-messages:
